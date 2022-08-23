a11db811e5f5213e45d560e1e4555002a6071187
brandenwoods <brandendwoods@gmail.com>
2022-08-22 23:06:31 -0600

    fixed game function, cleaned up playround function

--- javascript.js
+++ javascript.js
@@ -1,30 +1,54 @@
-let playerInput1 = prompt('Test your luck! Input Rock, Paper, or Scissors:', );
-let playerInput = playerInput1.toLowerCase();
-if (playerInput === 'rock' || playerInput === 'paper' || playerInput === 'scissors') {
-   console.log(playerInput);
+let playerSelectionPrompt = prompt('Test your luck! Input Rock, Paper, or Scissors:', );
+let playerSelection = playerSelectionPrompt.toLowerCase();
+if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+   console.log(playerSelection);
 }
- else {
+    else {
     alert('You didnt enter your choice correctly!')
  } // Determines if player inputted their play correctly; returns error message if they didn't.
 
-let computerChoiceArray = ['rock', 'paper', 'scissors'];
-let computerInput = computerChoiceArray[Math.floor(Math.random() *computerChoiceArray.length)]; // Determines play for computer.
+function getComputerChoice(arr) {
+const randomIndex = Math.floor(Math.random() * array.length);
+const item = arr[randomIndex];
+return item;
+}
+const array = ['rock', 'paper', 'scissors'];
+const computerSelection = getComputerChoice(array);
+console.log(computerSelection);
 
-console.log(computerInput);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {    
+        return 2;
+         } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+         return 1;
+         } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+         return 0;
+         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+         return 1;
+         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+         return 1;
+         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+         return 0;
+         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+         return 1;
+         }
+  }
 
+  let wL = playRound(playerSelection, computerSelection);
 
- if (playerInput === computerInput) {
-  console.log('Its a tie! Play again');
-   } else if (playerInput === 'rock' && computerInput === 'scissors') {
-     console.log('You win! Computer played scissors.');
-   } else if (playerInput === 'rock' && computerInput === 'paper') {
-    console.log('You lose! Computer played paper.');
-   } else if (playerInput === 'paper' && computerInput === 'rock') {
-    console.log('You win! Computer played rock.');
-   } else if (playerInput === 'paper' && computerInput === 'scissors') {
-    console.log('You lose! Computer played scissors');
-   } else if (playerInput === 'scissors' && computerInput === 'rock') {
-    console.log('You lose! Computer played rock.');
-   } else if (playerInput === 'scissors' && computerInput === 'paper') {
-    console.log('You win! Computer played paper.');
-   }
+  console.log(wL);
+
+  function game(wL) {
+    for (let i = 0; i < 5; i++) {
+        if (wL === 1) {
+            return 'Win for player';
+        } else if (wL === 0) {
+            return 'Loss for player';
+        } else if (wL === 2) {
+            return 'It was a tie!'
+        }
+    }
+}
+let winTally = 0;
+
+console.log(game(wL));
